f6519cae3152bf89e58dee0756ab60336ee517b1
hithem <hithem.itsoft@gmail.com>
2019-03-14 20:14:49 +0200

    angular login security

---
+++ src/app/_services/authentication.service.ts
@@ -0,0 +1,42 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { User } from '../_models';
+
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class AuthenticationService {
+    constructor(private http: HttpClient) { }
+
+    login(username: string, password: string) {
+
+        console.log(btoa("password"));
+
+        // store user details and basic auth credentials in local storage
+        // to keep user logged in between page refreshes
+        let user : User = new User();
+        user.firstName = username;
+        user.authdata = window.btoa(username + ':' + password);
+        localStorage.setItem('currentUser', JSON.stringify(user));
+
+        return this.http.get<any>(`/resource`).pipe(map(user => {
+                                                               // login successful if there's a user in the response
+                                                                console.log(user);
+                                                               return user;
+                                                           }));;
+    }
+
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('currentUser');
+    }
+
+    checkAccess() {
+            return this.http.get<any>(`/resource`).pipe(map(user => {
+                                                                   // login successful if there's a user in the response
+                                                                    console.log(user);
+                                                                   return user;
+                                                               }));;
+    }
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '../_models';
+import { AuthenticationService } from '../_services';
+
+@Component({templateUrl: 'home.component.html'})
+export class HomeComponent implements OnInit {
+    currentUser: User;
+
+
+    constructor(private authenticationService : AuthenticationService) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+
+    ngOnInit() {
+        this.checkAccess();
+    }
+
+    private checkAccess() {
+        this.authenticationService.checkAccess().pipe(first()).subscribe(users => {
+
+        });
+    }
+}
